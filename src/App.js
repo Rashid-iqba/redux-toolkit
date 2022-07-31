83ce9c2fca310d1f87ab9e8512cbadc0bbf75b31
RASHID IQBAL <45360835+Rashid-iqba@users.noreply.github.com>
2022-07-31 18:15:44 +0530

    wd

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React,{useState} from "react";
 import Login from "./components/Login";
 import Home from "./components/Home";
 
+import {useSelector,useDispatch} from "react-redux";
+import {Increment,Decrement } from "./reduxs/actions/index";
+
 const App = () => {
+  const mystate = useSelector( (state) => state.changeTheNumber);
+  const dispatch = useDispatch();
+  // const [first, setfirst] = useState(0)
 return(
   <>
-  <Login/>
+  {/* <Login/> */}
+
+  <div style={{flex: 1,flexDirection:"row"}}>
+  <button onClick={()=> dispatch(Increment())  } className="btn btn-primary" type="submit">Increment + </button>
+     <div>{mystate}</div>
+   <button onClick={()=> dispatch(Decrement()) } className="btn btn-primary" type="submit">Decrement - </button>
+        
+  </div>
   </>
 )
 };
